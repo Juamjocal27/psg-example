fc46a36d357765907c58a01bc905ed7be76b5174
Juan Jose Caparicona Marca <juamjocal27@gmail.com>
2024-05-13 16:55:37 -0400

    Sesion 4

---
+++ sesion04.py
@@ -0,0 +1,12 @@
+# Example of execution flow
+# Instruction 1
+print("Start of the program")
+
+# Instruccion 2
+print("Print second instruction")
+
+# Instruccion 3
+print("Hello, world!")
+
+# Instruccion 4
+print("End of the program")
