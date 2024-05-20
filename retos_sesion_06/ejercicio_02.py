d5d1b80f3700b3e65dc80fe0bce7fbdb70fa085f
Juan Jose Caparicona Marca <juamjocal27@gmail.com>
2024-05-19 20:33:36 -0400

    Ejercicio 03

--- retos_sesion_06/ejercicio_02.py
+++ retos_sesion_06/ejercicio_02.py
@@ -4,3 +4,12 @@ print(not(True != True))
 print(not(True != False))
 print(not(False != True))
 print(not(False != False))
+
+def xor(a, b):
+    xor = a and not b or not a and b
+    return xor
+print(not(xor(True, True)))
+print(not(xor(True, False)))
+print(not(xor(False, True)))
+print(not(xor(False, False)))
+
