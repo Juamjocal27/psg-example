1697f3314ecef59a7cfe21a3bb89c97226437781
Juan Jose Caparicona Marca <juamjocal27@gmail.com>
2024-05-21 15:12:37 -0400

    Ejercicio 5

--- retos_sesion_07/ejercicio_05.py
+++ retos_sesion_07/ejercicio_05.py
@@ -1,7 +1,17 @@
 palabra = "Anita lava la tina"
 
-#cadena = palabra.lower()
-#cadena = ''.join(char for char in cadena if char.isalnum())
-cadena = palabra.strip(" ")
-print(cadena)
-print(cadena == cadena[::-1])
+palabra = palabra.lower()
+palabra = palabra.split()
+palabra = ''.join(palabra)
+print(palabra == palabra[::-1])
+# Convertimos toda la palabra en minusculas
+#palabra = palabra.lower()
+# print(palabra)
+# Separamos la lista donde haya un espacio
+# palabra = palabra.split()
+# Unimos la cadena sin espacios
+# ca = ''.join(cadena)
+# print(palabra == cadena[::-1])
+# cadena = ''.join(char for char in cadena if char.isalnum())
+# print(cadena)
+# print(palabra == cadena[::-1])
