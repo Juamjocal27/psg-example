54bbadeb4212844fcc0e9f9363e5b165b80991fc
Juan Jose Caparicona Marca <juamjocal27@gmail.com>
2024-06-03 23:26:09 -0400

    Session 12

---
+++ sesion12.py
@@ -0,0 +1,55 @@
+# print ("Inicio")
+# condicion = True
+# if condicion:
+#     # Bloque de código
+#     print ("Cumple condición")
+# print ("Fin")
+
+# print ("Inicio")
+# numero = 4
+# if numero % 2 == 0: # Si el módulo de 2 es 0
+#     print ("El número es par")
+# print ("Fin")
+
+# print ("Inicio")
+# condicion = False
+# if condicion:
+#     # Bloque de código
+#     print ("Cumple condición")
+# else:
+#     # Bloque de código
+#     print ("No cumple condición")
+# print ("Fin")
+
+# print ("Inicio")
+# numero = 3
+# if numero % 2 == 0: # Si el módulo de 2 es 0
+#     print ("El número es par")
+# else:
+#     print ("El número es impar")
+# print ("Fin")
+
+# print ("Inicio Anidado")
+# condicion_1 = True
+# condicion_2 = False
+# if condicion_1:
+#     print ("Cumple condición 1")
+#     if condicion_2:
+#         print ("Cumple condición 2")
+#     else:
+#         print ("No cumple condición 2")
+# else:
+#     print ("No cumple condición 1")
+# print ("Fin")
+
+# print ("Inicio Par, Impar o Cero")
+# numero = 0  
+# if numero > 0 or numero < 0:
+#     if numero % 2 == 0: # Si el módulo de 2 es 0
+#         print ("El número es par")
+#     else:
+#         print ("El número es impar")
+# else:
+#     print ("El número es cero")
+# print ("Fin")
+
