352eb1e8f2fdb08b1a2d6139c33ac117c395e963
Juan Jose Caparicona Marca <juamjocal27@gmail.com>
2024-06-05 12:53:06 -0400

    actualizado

--- retos_sesion_13/ejercicio_05.py
+++ retos_sesion_13/ejercicio_05.py
@@ -1,14 +1,12 @@
 # Imprimir un tablero de ajedre de 8x8 con los caracteres '#' y '*'
 
-# Dimensiones del tablero
 filas = 8
 columnas = 8
 
-# Crear el tablero de ajedrez
 for i in range(filas):
     for j in range(columnas):
         if (i + j) % 2 == 0:
             print('#', end='')
         else:
             print('*', end='')
-    print()  # Nueva línea después de cada fila
+    print()
